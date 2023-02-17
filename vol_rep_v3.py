648984c97df243b18288842c7ba4b9edbc7b108f
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-02-17 09:41:49 +0000

    as

--- vol_rep_v3.py
+++ vol_rep_v3.py
@@ -52,9 +52,10 @@ if len(txt)>0:
             with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
                 # Write each dataframe to a different worksheet.
                 for i in range(number):
-                    df_split[i].to_excel(writer, sheet_name=str(i), header=False, index=False)
                     if i==0:
                         sheet_name='K'
+                    df_split[i].to_excel(writer, sheet_name=str(i), header=False, index=False)
+
 
                 # df_split[0].to_excel(writer, sheet_name='K', header=False, index=False)
                 # df_split[1].to_excel(writer, sheet_name='Y', header=False, index=False)
