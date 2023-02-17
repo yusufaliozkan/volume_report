be387c99d7d45e8dcd2c3fecb7eedb84ebf2590c
Yusuf Ozkan <70472797+YusufAliOzkan@users.noreply.github.com>
2023-02-17 09:05:03 +0000

    sd

--- vol_rep_v3.py
+++ vol_rep_v3.py
@@ -41,6 +41,7 @@ if len(txt)>0:
             url = "{}"
             return '=HYPERLINK("%s", "%s")' % (url.format(value), value)
         df_url[2] = df_url[2].apply(lambda x: make_hyperlink(x))
+        number = st.number_input("How many sheets you'd like to create?")
         df_split = np.array_split(df_url, 2)
         if df_split is not None:
             buffer = io.BytesIO()
